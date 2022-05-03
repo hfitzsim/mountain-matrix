a162ea000d14a200d0addc062606b7989c1d8e2d
Hannah Fitzsimmons <hannahfitzsimmons@gmail.com>
2022-05-02 21:05:20 -0400

    adding a hamburger menu to toggle the sidenav

--- mtn-matrix/src/app/app.module.ts
+++ mtn-matrix/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 // COMPONENTS
 import { AppComponent } from './app.component';
