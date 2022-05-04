d4498b96df1cc9a533bfe70498fd4f2faa1621b1
Hannah Fitzsimmons <hannahfitzsimmons@gmail.com>
2022-05-04 12:57:21 -0400

    minor updates to the map setview

--- mtn-matrix/src/app/services/map.service.ts
+++ mtn-matrix/src/app/services/map.service.ts
@@ -20,11 +20,11 @@ export class MapService {
     navigator.geolocation.getCurrentPosition((position) => {
       const coords = [position.coords.latitude, position.coords.longitude];
 
-      let map = L.map('map').setView(coords, 5);
+      let map = L.map('map').setView(coords, 6);
 
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGZpdHpzaW0iLCJhIjoiY2wybmozdzFsMHhjMjNiczMxcjYwN2FlbyJ9.eBnOgWl7LtvS_7gD6A8g9Q', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
+        maxZoom: 20,
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
@@ -53,7 +53,7 @@ export class MapService {
     }, (err) => {
       console.log(err);
     }, {
-      enableHighAccuracy: false,
+      enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0
     })
